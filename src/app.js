3dd975ea401ffd3ddde821fe1e3a429a714a86bf
Suchetha15 <b.suchetha@gmail.com>
2018-08-06 13:38:54 +0530

    setup prod webpack build

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './router/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
+// import { addExpense } from './actions/expenses';
+// import { setTextFilter } from './actions/filters';
+// import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
+
 
 const store = configureStore();
 // console.log(store.getState());
